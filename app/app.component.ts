6d45eeb7ddf4e3e845e56e311e0915668a1607a9
Gizzz <bulatkhusainov@yandex.ru>
2016-10-28 21:06:50 +0300

    import todo-app assets, add abilitity to add or remove todo

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,49 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+	moduleId: module.id,
+	selector: 'my-app',
+	templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+	todos = [
+		{
+			id: 1,
+			title: "first",
+			completed: true,
+		}, {
+			id: 2,
+			title: "second",
+			completed: false,
+		}
+	];
+
+	deleteTodo(id: number) {
+		let todoToDelete = this.todos.filter(item => item.id === id)[0];
+
+		if (todoToDelete) {
+			let index = this.todos.indexOf(todoToDelete);
+			this.todos.splice(index, 1)
+		}
+	}
+
+	createTodo(title: string) {
+		let maxId = 0;
+
+		for (let i = 0; i < this.todos.length; i++) {
+			if (this.todos[i].id > maxId) {
+				maxId = this.todos[i].id;
+			}
+		}
+
+		let newId = maxId + 1;
+
+		let todoToCreate = {
+			id: newId,
+			title,
+			completed: false,
+		};
+
+		this.todos.push(todoToCreate);
+	}
+}
