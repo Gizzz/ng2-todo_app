6bdf90e4e03a685b74ad83971a4f6481812a430a
Gizzz <bulatkhusainov@yandex.ru>
2016-12-28 21:10:03 +0300

    add e2e tests for clear-completed button

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,13 +6,11 @@ let todos = todoApp.elementArrayFinder_.all(by.css(".todo-list li"));
 describe("TodoApp", function () {
   beforeEach(function () {
     browser.get('');
-
     expect(todos.count()).toBe(0);
   });
 
   afterEach(function () {
     // remove todos from previous test
-
     todos.count().then(count => {
       for (let i = count; i > 0; i--) {
         removeLastTodo();
@@ -20,70 +18,77 @@ describe("TodoApp", function () {
     });
   });
 
-  it("should create new todo", function () {
-    let expectedText = "tst";
-    createTodo({ title: expectedText });
+  describe("basic ops", function() {
+    it("should create todo with title", function () {
+      let expectedText = "tst";
+      createTodo({ title: expectedText });
 
-    expect(todos.count()).toBe(1);
-    expect(todos.last().getText()).toBe(expectedText);
-  });
+      expect(todos.count()).toBe(1);
+      expect(todos.last().getText()).toBe(expectedText);
+    });
 
-  it("should remove todo", function () {
-    createTodo({ title: "tst" });
-    expect(todos.count()).toBe(1);
+    it("should not create todo without title", function () {
+      createTodo({ title: "" });
+      expect(todos.count()).toBe(0);
+    });
 
-    removeLastTodo();
-    expect(todos.count()).toBe(0);
-  });
+    it("should remove todo", function () {
+      createTodo({ title: "tst" });
+      expect(todos.count()).toBe(1);
+
+      removeLastTodo();
+      expect(todos.count()).toBe(0);
+    });
 
 
-  it("should create todos in subsequent order", function () {
-    let todoTitle_1 = "todo title 1";
-    let todoTitle_2 = "todo title 2";
-    let todoTitle_3 = "todo title 3";
+    it("should create todos in subsequent order", function () {
+      let todoTitle_1 = "todo title 1";
+      let todoTitle_2 = "todo title 2";
+      let todoTitle_3 = "todo title 3";
 
-    createTodo({ title: todoTitle_1 });
-    createTodo({ title: todoTitle_2 });
-    createTodo({ title: todoTitle_3 });
-    expect(todos.count()).toBe(3);
+      createTodo({ title: todoTitle_1 });
+      createTodo({ title: todoTitle_2 });
+      createTodo({ title: todoTitle_3 });
+      expect(todos.count()).toBe(3);
 
-    let lastTodo = todos.last();
-    expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_3);
-    removeLastTodo();
+      let lastTodo = todos.last();
+      expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_3);
+      removeLastTodo();
 
-    lastTodo = todos.last();
-    expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_2);
-    removeLastTodo();
-    
-    lastTodo = todos.last();
-    expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_1);
-    removeLastTodo();
+      lastTodo = todos.last();
+      expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_2);
+      removeLastTodo();
+      
+      lastTodo = todos.last();
+      expect(lastTodo.element(by.css("label")).getText()).toBe(todoTitle_1);
+      removeLastTodo();
 
-    expect(todos.count()).toBe(0);
-  });
+      expect(todos.count()).toBe(0);
+    });
 
-  it("should mark todo as completed", function () {
-    createTodo({ title: "tst" });
-    expect(todos.count()).toBe(1);
+    it("should mark todo as completed", function () {
+      createTodo({ title: "tst" });
+      expect(todos.count()).toBe(1);
 
-    let createdTodo = element(by.css(".todo-list li"));
-    let completedCheckbox = createdTodo.element(by.css("input.toggle"));
-    expect(completedCheckbox.isSelected()).toBe(false);
+      let createdTodo = element(by.css(".todo-list li"));
+      let completedCheckbox = createdTodo.element(by.css("input.toggle"));
+      expect(completedCheckbox.isSelected()).toBe(false);
 
-    completedCheckbox.click();
-    expect(completedCheckbox.isSelected()).toBe(true);
-  });
+      completedCheckbox.click();
+      expect(completedCheckbox.isSelected()).toBe(true);
+    });
 
-  it("should mark todo as active", function () {
-    createTodo({ title: "tst", completed: true });
-    expect(todos.count()).toBe(1);
+    it("should mark todo as active", function () {
+      createTodo({ title: "tst", completed: true });
+      expect(todos.count()).toBe(1);
 
-    let createdTodo = todos.last();
-    let completedCheckbox = createdTodo.element(by.css("input.toggle"));
-    expect(completedCheckbox.isSelected()).toBe(true);
+      let createdTodo = todos.last();
+      let completedCheckbox = createdTodo.element(by.css("input.toggle"));
+      expect(completedCheckbox.isSelected()).toBe(true);
 
-    completedCheckbox.click();
-    expect(completedCheckbox.isSelected()).toBe(false);
+      completedCheckbox.click();
+      expect(completedCheckbox.isSelected()).toBe(false);
+    });
   });
 
   describe("completeAll button", function () {
@@ -149,17 +154,34 @@ describe("TodoApp", function () {
       expect(completedCheckbox_2.isSelected()).toBe(false);
     });
   });
+
+  describe("clearCompleted button", function () {
+    let clearCompleted_btn = todoApp.$("button.clear-completed");
+
+    it("should not be visible if no completed todos", function () {
+      expect(clearCompleted_btn.isPresent()).toBe(false);
+
+      createTodo({ title: "1", completed: false });
+      expect(clearCompleted_btn.isPresent()).toBe(false);
+    });
+
+    it("should be visible if any completed todo", function () {
+      expect(clearCompleted_btn.isPresent()).toBe(false);
+      createTodo({ title: "1", completed: true });
+      expect(clearCompleted_btn.isPresent()).toBe(true);
+    });
+  });
 });
 
 // helpers
 
-function createTodo(options: any): void {
-  let title = options.title;
+function createTodo(todoOptions: any): void {
+  let title = todoOptions.title;
   let newTodoInput = todoApp.element(by.css(".new-todo"));
   newTodoInput.sendKeys(title);
   newTodoInput.sendKeys(protractor.Key.ENTER);
 
-  if (options.completed) {
+  if (todoOptions.completed) {
     let createdTodo = todos.last();
     let completedCheckbox = createdTodo.element(by.css("input.toggle"));
     completedCheckbox.click();
